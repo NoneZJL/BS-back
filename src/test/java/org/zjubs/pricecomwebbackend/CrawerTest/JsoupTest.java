364a110d414e4e25bd29d8791c5a6a91a8e2fd06
xftong <1290217090@qq.com>
2024-12-23 09:13:24 +0800

    [delete]注释爬虫测试

--- src/test/java/org/zjubs/pricecomwebbackend/CrawerTest/JsoupTest.java
+++ src/test/java/org/zjubs/pricecomwebbackend/CrawerTest/JsoupTest.java
@@ -18,37 +18,37 @@ public class JsoupTest {
     private JdService jdService;
 
 
-    @Test
-    public void testGetUrl() {
-        ApiResult apiResult = jdService.searchAndInsert("餐巾纸");
-        if (apiResult.ok) {
-            System.out.println("成功");
-        } else {
-            System.out.println("失败");
-        }
-    }
-
-    @Test
-    public void snTest() {
-        List<Good> search = SnCrawlerUtil.search("电脑");
-        System.out.println(search);
-    }
-
-    @Test
-    public void wphTest() {
-        List<Good> search = WphCrawlerUtil.search("手机");
-        System.out.println(search);
-    }
-
-    @Test
-    public void singleTest() {
-        String url = "https://www.xiaomiyoupin.com/detail?gid=153253";
-        Double price = WphCrawlerUtil.getSingleItemPrice(url);
-        System.out.println("获取的价格为 " + price + " 元");
-    }
-
-    @Test
-    public void singleGoodTest() {
-        JDCrawlerUtil.getSingleGoodPrice("https://item.jd.com/100115333286.html");
-    }
+//    @Test
+//    public void testGetUrl() {
+//        ApiResult apiResult = jdService.searchAndInsert("餐巾纸");
+//        if (apiResult.ok) {
+//            System.out.println("成功");
+//        } else {
+//            System.out.println("失败");
+//        }
+//    }
+//
+//    @Test
+//    public void snTest() {
+//        List<Good> search = SnCrawlerUtil.search("电脑");
+//        System.out.println(search);
+//    }
+//
+//    @Test
+//    public void wphTest() {
+//        List<Good> search = WphCrawlerUtil.search("手机");
+//        System.out.println(search);
+//    }
+//
+//    @Test
+//    public void singleTest() {
+//        String url = "https://www.xiaomiyoupin.com/detail?gid=153253";
+//        Double price = WphCrawlerUtil.getSingleItemPrice(url);
+//        System.out.println("获取的价格为 " + price + " 元");
+//    }
+//
+//    @Test
+//    public void singleGoodTest() {
+//        JDCrawlerUtil.getSingleGoodPrice("https://item.jd.com/100115333286.html");
+//    }
 }
